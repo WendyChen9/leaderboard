edbb6a9ebc35dc486b5fe4d727e8100f43ce5025
WendyChen9 <31325518+WendyChen9@users.noreply.github.com>
2017-10-11 17:38:08 -1000

    Task 6: Database Part 2

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -3,21 +3,33 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({}, {sort: {score: -1, name: 1}});
     },
     'selectedClass': function(){
-      var playerId = this._id;
-      var selectedPlayer = Session.get('selectedPlayer');
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
       if(playerId == selectedPlayer) {
         return "selected";
       }
+    },
+    'selectedPlayer': function(){
+      let selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne({_id: selectedPlayer});
     }
   });
   Template.leaderboard.events({
     'click .player': function(){
-      var playerId = this._id;
+      let playerId = this._id;
       Session.set('selectedPlayer', playerId);
 
+    },
+    'click .increment': function(){
+      let selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({_id: selectedPlayer},{ $inc:{score: 5}});
+    },
+    'click .decrement': function(){
+      let selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({_id: selectedPlayer},{$inc: {score: -5}});
     }
   });
 
